11564281036b91549f398db79a401334d2410ee2
Chloé Bidau <108899322+Clochettenb@users.noreply.github.com>
2023-03-02 17:00:25 +0100

    Add files via upload

---
+++ Movies/IndianaJones.java
@@ -0,0 +1,12 @@
+class IndianaJones {
+    public static void main(String[] args) {
+        String title = "Indiana Jones and the Last Crusade";
+        boolean haveSeenMovie = true;
+        int date = 1989;
+        double imdbNote = 8.2;
+        System.out.println(title);
+        System.out.println(haveSeenMovie);
+        System.out.println(date);
+        System.out.println(imdbNote);
+    }
+}
