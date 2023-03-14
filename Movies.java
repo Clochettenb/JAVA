582dcd32bc7268971d943132ce6a558acdae921d
Chloé Bidau <108899322+Clochettenb@users.noreply.github.com>
2023-03-14 16:52:29 +0100

    Add files via upload

---
+++ Movies.java
@@ -0,0 +1,9 @@
+class Movies {
+    public static void main(String[] args) {
+        String[][] titles = new String[][] {
+                {"Indiana Jones and the Kingdom of the Crystal Skull", "Indiana Jones and the Last Crusade", "Indiana Jones and the Temple of Doom"},
+                {"knife", "shield"},
+                {"belt", "dagger", "gun", "shield"}
+        };
+    }
+}
