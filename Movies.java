e9e76a3c571b1ebdca4e8cfbc8b95ce58315151b
Chloé Bidau <108899322+Clochettenb@users.noreply.github.com>
2023-03-14 17:15:36 +0100

    Add files via upload

--- Movies.java
+++ Movies.java
@@ -1,16 +1,17 @@
 class Movies {
     public static void main(String[] args) {
-        String[] titles = new String[]{"Indiana Jones and the Kingdom of the Crystal Skull", "Indiana Jones and the Last Crusade", "Indiana Jones and the Temple of Doom"};
-        String[][] actors = new String[][]{
-                {titles[0], "actor 1", "actor 2", "actor 3"},
-                {titles[1], "actor 1", "actor 2", "actor 3"},
-                {titles[2], "actor 1", "actor 2", "actor 3"}
+        String[] titles = {"Indiana Jones and the Kingdom of the Crystal Skull", "Indiana Jones and the Last Crusade", "Indiana Jones and the Temple of Doom"};
+        String[][] actors = {
+                {"actor 1", "actor 2", "actor 3"},
+                {"actor 1", "actor 2", "actor 3"},
+                {"actor 1", "actor 2", "actor 3"}
         };
-        for (String title:titles
-             ) {
-            System.out.println("Dans le film" ${title}", les principaux acteurs sont" ${actors[0]} + ${actors[1]} + ${actors[2]})
-        };
-        System.out.println(titles);
-        System.out.println(actors);
+        for (int i = 0; i < titles.length; i++) {
+            System.out.print("Dans le film " + titles[i] + ", les principaux acteurs sont : ");
+            for (int j = 0; j < actors[i].length; j++) {
+                System.out.print(actors[i][j] + ", ");
+            }
+            System.out.println("");
+        }
     }
 }
