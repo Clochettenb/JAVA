b7d8186ed051e6342b14ac0d493dff324cbf3848
Chloé Bidau <108899322+Clochettenb@users.noreply.github.com>
2023-03-16 10:30:37 +0100

    Add files via upload

--- Decipherer.java
+++ Decipherer.java
@@ -1,17 +1,16 @@
-class Decipher {
+class Decipherer {
+
+    public static String decipher(String code) {
+        int key = code.length() / 2;
+        String subString = code.substring(5, 5 + key);
+        subString = subString.replace("@#?", " ");
+        return new StringBuilder(subString).reverse().toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(decipher("0@sn9sirppa@#?ia'jgtvryko1"));
         System.out.println(decipher("q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj"));
         System.out.println(decipher("aopi?sedohtém@#?sedhtmg+p9l!"));
 
-    }
-
-    public static String decipher(String code) {
-        int key = code.length() / 2;
-        String subString = code.substring(5, key+5);
-        subString = subString.replace("@#?", " ");
-        StringBuilder sb = new StringBuilder(subString);
-        sb.reverse();
-        return sb.toString();
     }
 }
